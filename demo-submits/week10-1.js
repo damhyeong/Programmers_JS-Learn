6e8c7201dc49206b2cbb292e77890453ac9dc2a8
damhyeong <rhdwhdals8@naver.com>
2024-10-22 01:22:02 +0900

    Complete demo-submits At Week10 Part1

--- demo-submits/week10-1.js
+++ demo-submits/week10-1.js
@@ -1,3 +1,19 @@
 const validate = (value) => {
     return value instanceof Number ? value : null;
 }
+
+let age = 10;
+
+function addAge(age) {
+    return age + 3;
+}
+
+addAge(age);
+
+const foo = (arg) => {
+    return arg();
+}
+
+foo(() => {
+    return 11;
+})
